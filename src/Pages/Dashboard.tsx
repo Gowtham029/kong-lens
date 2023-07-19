fb006790ffc8ad46efc298aef6bbdc9880888e24
aravinthkumarms <aravinthkumarse@gmail.com>
2023-07-19 16:44:28 +0530

    resolve lint warnings

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
